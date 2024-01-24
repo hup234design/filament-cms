80651a83dcbd9a6438a540593eeb6f524fc81547
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-01-24 10:10:59 +0000

    update placeholder header and footer

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,37 +1,37 @@
-<header class="py-8 bg-gray-600">
-    <div class="max-w-7xl mx-auto px-8 flex justify-between items-center">
+<header class="p-8 bg-gray-600">
+    <div class="flex justify-between items-center">
         <div class="flex-shrink-0">
             <a href="{{ route('pages.home') }}" class="hover:cursor-pointer text-3xl font-bold text-white leading-none">
                 {{ config('app.name') }}
             </a>
         </div>
-            <div class="inline-block divide-y">
-            <ul class="pb-4 flex items-center justify-end divide-x-2 divide-gray-100">
-                <li class="leading-none px-4">
+        <div class="inline-block divide-y">
+            <ul class="pb-4 flex items-center justify-end divide-x divide-gray-100">
+                <li class="leading-none px-2">
                     <a href="{{ route('pages.home') }}" class="hover:cursor-pointer text-gray-100 font-semibold text-lg leading-none">
                         Home
                     </a>
                 </li>
                 @foreach( \Hup234design\FilamentCms\Models\Page::where('is_home',false)->visible()->pluck('title','slug') as $slug=>$title)
-                    <li class="leading-none px-4">
+                    <li class="leading-none px-2">
                         <a href="{{ route('pages.page', $slug) }}" class="hover:cursor-pointer text-gray-100 font-semibold text-lg leading-none">
                             {{ $title }}
                         </a>
                     </li>
                 @endforeach
             </ul>
-            <ul class="pt-4 flex items-center justify-end divide-x-2 divide-gray-100">
-                <li class="leading-none px-4">
+            <ul class="pt-4 flex items-center justify-end divide-x divide-gray-100">
+                <li class="leading-none px-2">
                     <a href="{{ route('services.index') }}" class="hover:cursor-pointer text-gray-100 font-semibold text-lg leading-none">
                         Services
                     </a>
                 </li>
-                <li class="leading-none px-4">
+                <li class="leading-none px-2">
                     <a href="{{ route('testimonials.index') }}" class="hover:cursor-pointer text-gray-100 font-semibold text-lg leading-none">
                         Testimonials
                     </a>
                 </li>
-                <li class="leading-none px-4">
+                <li class="leading-none px-2">
                     <a href="{{ route('posts.index') }}" class="hover:cursor-pointer text-gray-100 font-semibold text-lg leading-none">
                         Posts
                     </a>
@@ -39,7 +39,4 @@
             </ul>
         </div>
     </div>
-
-
-
 </header>
